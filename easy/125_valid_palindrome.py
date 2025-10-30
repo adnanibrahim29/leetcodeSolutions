4f2804e4ac94f4527a4c485bee5d255723db8b38
u230408 <adnan.ibrahim.2023@mumail.ie>
2025-10-30 21:16:50 +0000

    125 method 2 using two pointers

--- easy/125_valid_palindrome.py
+++ easy/125_valid_palindrome.py
@@ -2,6 +2,7 @@
 Problem 125: Valid Palindrome
 """
 
+# Method 1: Using Reverse
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         output = ''
@@ -10,3 +11,26 @@ class Solution:
             if i.isalnum():
                 output += i.lower()
         return output == output[::-1]
+
+# Method 2: Using Two Pointers
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l, r = l + 1, r - 1
+        return True
+
+
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord("Z") or
+                ord('a') <= ord(c) <= ord("z") or
+                ord('0') <= ord(c) <= ord("9"))   
