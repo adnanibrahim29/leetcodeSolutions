ff4c7423d610dcd59541a1afca57658d0132e27c
u230408 <adnan.ibrahim.2023@mumail.ie>
2025-10-30 20:20:04 +0000

    128 Longest Consequtive number

---
+++ medium/128_longest_consequtive_number.py
@@ -0,0 +1,16 @@
+"""
+Problem 128: Longest Consequtive Number
+"""
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numsSet = set(nums)
+        longest = 0
+
+        for i in nums:
+            if (i - 1) not in numsSet:
+                length = 0
+                while (i + length) in numsSet:
+                    length += 1
+                longest = max(longest, length)
+        return longest
